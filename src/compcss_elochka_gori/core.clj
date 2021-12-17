943cac8fe918787ee2eaccdc5590da8cc843e3c4
rnikolae <1233sasha@gmail.com>
2021-12-17 20:16:32 +0300

    Create

---
+++ src/compcss_elochka_gori/core.clj
@@ -0,0 +1,70 @@
+(ns compcss-elochka-gori.core
+  (:require [clojure.string]
+            [clojure.java.io]))
+
+(defn elochka-create
+  [blocks]
+  (let [max-w 300]
+    (loop [nodes   (seq blocks)
+           prev    ""
+           current (first blocks)
+           result  (first blocks)
+           index   15]
+      (if (seq nodes)
+        (let [node (first nodes)]
+          (cond
+            (> (count current)
+               (count prev))
+            (recur (next nodes)
+                   current
+                   node
+                   (str result "\n" current)
+                   index)
+            (> (count current) index)
+            (do (prn (count current))
+                (recur (next nodes)
+                       node
+                       node
+                       result
+                       (if (< (count current) max-w)
+                         (count current)
+                         max-w)))
+            :else
+            (recur (next nodes)
+                   prev
+                   (str current node)
+                   result
+                   index)))
+        result))))
+
+(defn css-blocks [css]
+  (-> css
+      (clojure.string/replace
+       #";|,|\{|}"
+       {";" ";\n"
+        "," ",\n"
+        "{" "{\n"
+        "}" "}\n"})
+      (clojure.string/split #"\n")))
+
+(defn center-computing [max-size vetka]
+  (-> max-size
+      (- (Math/ceil (/ (count vetka) 2)))
+      (repeat " ")
+      (clojure.string/join)
+      (str vetka)))
+
+(defn elochka-centering [elochka]
+  (->> (clojure.string/split elochka #"\n")
+       (map (partial center-computing (-> (sort-by count elochka) last count)))
+       (clojure.string/join "\n")))
+
+(defn elochka-gori [css]
+  (-> (css-blocks css)
+      (elochka-create)
+      (elochka-centering)))
+
+(defn middleware
+  [condiguration db]
+  "da")
+
