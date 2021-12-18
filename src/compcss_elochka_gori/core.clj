caf8373a7565abffb3695d703d39ffbcfcbaedf4
panthevm <panthevm@yandex.ru>
2021-12-18 15:38:51 +0300

    Commit

--- src/compcss_elochka_gori/core.clj
+++ src/compcss_elochka_gori/core.clj
@@ -13,22 +13,21 @@
       (if (seq nodes)
         (let [node (first nodes)]
           (cond
-            (> (count current)
-               (count prev))
+            (> (count (str current))
+               (count (str prev)))
             (recur (next nodes)
                    current
                    node
                    (str result "\n" current)
                    index)
             (> (count current) index)
-            (do (prn (count current))
-                (recur (next nodes)
-                       node
-                       node
-                       result
-                       (if (< (count current) max-w)
-                         (count current)
-                         max-w)))
+            (recur (next nodes)
+                   node
+                   node
+                   result
+                   (if (< (count (str current)) max-w)
+                     (count (str current))
+                     max-w))
             :else
             (recur (next nodes)
                    prev
@@ -49,14 +48,14 @@
 
 (defn center-computing [max-size vetka]
   (-> max-size
-      (- (Math/ceil (/ (count vetka) 2)))
+      (- (Math/ceil (/ (count (str vetka)) 2)))
       (repeat " ")
       (clojure.string/join)
       (str vetka)))
 
 (defn elochka-centering [elochka]
   (->> (clojure.string/split elochka #"\n")
-       (map (partial center-computing (-> (sort-by count elochka) last count)))
+       (map (partial center-computing (-> (sort-by (comp count str) elochka) last count)))
        (clojure.string/join "\n")))
 
 (defn elochka-gori [css]
@@ -65,8 +64,10 @@
       (elochka-centering)))
 
 (defn middleware
-  [condiguration db]
-  (-> (slurp "some-file ") 
-      (elochka-gori) 
-      (spit "some-file")))
+  [configuration db]
+  (->>
+   (:compcss.core/output-stylesheets db)
+   (clj-ph-css.core/schema->string)
+   (elochka-gori)
+   (spit (get-in configuration [:output :css]))))
 
